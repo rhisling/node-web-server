a0ba1ed0ac5b091b763d70fb8162ebd4266aa8e1
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-06-09 23:51:00 -0700

    setiing env variable for heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
 
+const port = process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
@@ -48,4 +49,6 @@ app.get("/bad", (req, res) => {
   res.send({ errorMessage: "Unable to handle request" });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
