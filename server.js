f5fbac6e1d87db9aecdca4bbefab7ecbf9d482b2
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-06-09 23:41:18 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+
+app.use((req, res, next) => {
+  res.render("maintenance.hbs");
+});
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append to server.log");
+    }
+  });
+  next();
+});
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Home page",
+    welcomeMessage: "Welcome to my website"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({ errorMessage: "Unable to handle request" });
+});
+
+app.listen(3000);
